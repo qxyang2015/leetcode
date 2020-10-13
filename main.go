2a5e70bae338beb25430848099b31c1e12dd5da2
qxyang <Yang>
2020-10-13 21:52:04 +0800

    调整目录结构

--- main.go
+++ main.go
@@ -1,41 +1,5 @@
 package main
 
-import (
-	"fmt"
-	ReverseList "github.com/qxyang2015/Go/剑指offer/24.反转链表"
-)
-
 func main() {
-	l := &ReverseList.ListNode{
-		Val: 1,
-		Next: &ReverseList.ListNode{
-			Val: 2,
-			Next: &ReverseList.ListNode{
-				Val: 3,
-				Next: &ReverseList.ListNode{
-					Val: 4,
-					Next: &ReverseList.ListNode{
-						Val:  5,
-						Next: nil,
-					},
-				},
-			},
-		},
-	}
-	ShowList(l)
-	resL := ReverseList.ReverseList(l)
-	ShowList(resL)
-	fmt.Println("Done!")
-}
 
-func ShowList(l *ReverseList.ListNode) {
-	node := l
-	for {
-		if node == nil {
-			break
-		}
-		fmt.Print(node.Val, " ")
-		node = node.Next
-	}
-	fmt.Println()
 }
